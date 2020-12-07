3a2ae8d2484b2c9cb9422c6673715433fed1f5d9
Joelson Ramos Ferreira <47796454+joelsonRamos@users.noreply.github.com>
2020-12-07 09:34:22 -0300

    teste unitario

---
+++ tests/VerificaLinguagem.php
@@ -0,0 +1,11 @@
+<?php
+        namespace Tests;
+
+        use PHPUnit\Framework\TestCase;
+        use App\Compra;
+
+        class CompraTest extends TestCase
+        {
+
+            
+        }
